ca833dbd32612efa57294cc1fb4222c9e9a9b343
JPatao <jpatao@uci.edu>
2025-10-02 05:28:33 +0000

    Circle Class

--- HW1/src/mainClass.java
+++ HW1/src/mainClass.java
@@ -1,15 +1,30 @@
 abstract class Shape {
     String name;
     Shape(String newName) {name = newName;}
+    abstract void print();
+    abstract void draw();
+    abstract double area();
+}
+
+class Circle extends Shape {
+    double radius;
+    static final double PI = 3.14;
+    Circle(double newRadius) {
+        super("Circle");
+        radius = newRadius;
+    }
+    double area() {return PI * radius * radius;}
     void print() {
-        System.out.print(name);
+        System.out.print(super.name);
         System.out.print(" ( ");
+        System.out.print(radius);
         System.out.print(" ) ");
         System.out.print(" : ");
         System.out.print(Double.toString(area()));;
     }
-    void draw() {System.out.println("Shape.draw()");}
-    double area() {return 0.0;}
+    void draw() {
+        System.out.print("*");
+    }
 }
 
 /*public class mainClass {
